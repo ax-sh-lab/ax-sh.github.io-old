d835ed4373ea6d76e7e2134bfc7a120518df2955
Axmin <axmin@bottle.com.np>
2021-05-12 20:18:50 +0545

    made deploy work with vite [was a pain

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,10 @@ import reactRefresh from "@vitejs/plugin-react-refresh";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [reactRefresh()],
+  // build: {
+  //   outDir: "docs",
+  // },
   build: {
-    outDir: "docs",
+    outDir: "build",
   },
 });
