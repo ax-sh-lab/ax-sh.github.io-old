9edc356dd08d73b43b7548635a0507fef721c394
Axm <56112475+ax-sh@users.noreply.github.com>
2021-10-25 12:10:38 +0545

    feat: add prettier

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,22 +1,31 @@
-
-import logo from '../assets/logo.svg'
-import './App.scss'
+import logo from "../assets/logo.svg";
+import "./App.scss";
 import Layout from "./Layout/Layout";
 // @ts-ignore
-import data from '../assets/data.yml'
+import data from "../assets/data.yml";
 
+console.log(data);
 
+function Stacks({ list }: { list: string[] }) {
+  return (
+    <ul>
+      {list.map((item: string) => {
+        return <li>{item}</li>;
+      })}
+    </ul>
+  );
+}
 
 function App() {
-
   return (
     <>
       <Layout>
-          {data.name}
-          <img src={logo} alt={'logo'}/>
+        {data.name}
+        <Stacks list={data.stacks} />
+        <img src={logo} alt={"logo"} />
       </Layout>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
